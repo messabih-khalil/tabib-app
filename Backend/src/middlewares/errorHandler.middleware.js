884467acdec9a8a730c25d0cb0e8afe13f96efae
messabih-khalil <msbih.khalil@gmail.com>
2023-04-16 11:00:04 +0100

    feat(errorHandler.middleware.js): add error handling middleware to handle errors in production and development environments. The middleware sends error details to the client in production and logs the error in the hosting server. It also handles JWT errors and expired tokens.

---
+++ Backend/src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,54 @@
+const ResponseError = require('../middlewares/errorHandler.middleware');
+
+// Send Error To development
+const sendErrorDev = (err, res) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+// Send Error to production
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  } else {
+    // Log Error into Hosting server
+    console.error('Error', err);
+
+    // send Unkown Error to production
+
+    res.status(500).json({
+      success: false,
+      message: 'Somthing went very Worng!',
+    });
+  }
+};
+
+// Handle jwt error : incorrect token
+const handleJWTError = () =>
+  new ResponseError('Invalid Token , Please log in again!', 401);
+
+// Handle Expired jwt token
+
+const handleExpiredToken = () =>
+  new ResponseError('Your Token has expired , Please log in again!', 401);
+
+exports.errorHandler = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+  // Send Error details to client
+  if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleExpiredToken();
+    sendErrorProd(error, res);
+  }
+};
