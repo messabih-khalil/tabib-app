533fb12d154e289a0196fc6d408b929b4915a68f
messabih-khalil <msbih.khalil@gmail.com>
2023-04-16 10:42:31 +0100

    feat(users): add deleteMyAccount function to delete user account by id

---
+++ Backend/src/controllers/users/DELETE.js
@@ -0,0 +1,14 @@
+const User = require('../../models/user');
+const { catchAsync } = require('../../utils/catchAsync');
+
+// Delete My Account
+exports.deleteMyAccount = catchAsync(async (req, res, next) => {
+  
+  // * Get User and deleted
+  await User.findByIdAndDelete(req.userId);
+
+  res.status(204).json({
+    status: 'success',
+    message: 'Account deleted',
+  });
+});
