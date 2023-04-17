4c14df479dc0bfa197b0e4f059c8b1ec41b64241
messabih-khalil <msbih.khalil@gmail.com>
2023-04-17 15:45:28 +0100

    feat(signin_screen.dart): add SigninScreen widget with a form for user sign in.

---
+++ Mobile/lib/screens/signin_screen.dart
@@ -0,0 +1,46 @@
+import 'package:app/utils/colors.dart';
+import 'package:app/utils/spaces.dart';
+import 'package:app/widgets/shared/pop_screen.dart';
+import 'package:app/widgets/signin/sigin_form.dart';
+import 'package:flutter/material.dart';
+
+class SigninScreen extends StatelessWidget {
+  const SigninScreen({super.key});
+  static const route = '/signin';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: PopScreenWidget(),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppSpaces.container),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                'Sign in',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Welcom enter your phone number \nand password to sign in ",
+                style: TextStyle(
+                    color: AppColors.lightGreyColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SigninForm(),
+            ],
+          ),
+        ));
+  }
+}
