cd5755a95ab68782b4ce8ae4aaa9437231b5efb4
messabih-khalil <msbih.khalil@gmail.com>
2023-04-17 15:45:48 +0100

    feat(signup_screen.dart): add SignupScreen widget with a signup form and a back button.

---
+++ Mobile/lib/screens/signup_screen.dart
@@ -0,0 +1,37 @@
+import 'package:app/utils/spaces.dart';
+import 'package:app/widgets/shared/pop_screen.dart';
+import 'package:app/widgets/shared/input_label.dart';
+import 'package:app/widgets/signup/signup_form.dart';
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+  static const route = '/signup';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: PopScreenWidget(),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppSpaces.container),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                'Create Account',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SignupForm(),
+            ],
+          ),
+        ));
+  }
+}
