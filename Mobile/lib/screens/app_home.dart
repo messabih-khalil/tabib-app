70efe35e2fed6ead5f3e5b6a099fb0d9cf992892
messabih-khalil <msbih.khalil@gmail.com>
2023-04-18 19:32:45 +0100

    feat(app_home.dart): add specialist and doctor sections to home screen

--- Mobile/lib/screens/app_home.dart
+++ Mobile/lib/screens/app_home.dart
@@ -1,4 +1,8 @@
-import 'package:app/widgets/shared/pop_screen.dart';
+import 'package:app/utils/colors.dart';
+import 'package:app/utils/spaces.dart';
+import 'package:app/widgets/home/all_specialist_button.dart';
+import 'package:app/widgets/home/home_specialist.dart';
+import 'package:app/widgets/shared/doctor_card.dart';
 import 'package:flutter/material.dart';
 
 class AppHome extends StatelessWidget {
@@ -10,10 +14,64 @@ class AppHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: PopScreenWidget(),
+        automaticallyImplyLeading: false,
+        title: Text(
+          "Tabib",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
       ),
       body: Container(
-        child: Text("Home app"),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: AppSpaces.container),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                // Header
+                Container(
+                  height: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: AppColors.mainDarkColor,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                // Specialist
+                Text(
+                  'Specialist',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                HomeCategories(),
+                AllSpecialitiesButton(),
+                SizedBox(
+                  height: 5,
+                ),
+
+                SizedBox(
+                  height: 15,
+                ),
+                // Doctors
+                Text(
+                  'Doctors',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                DoctorCard()
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
