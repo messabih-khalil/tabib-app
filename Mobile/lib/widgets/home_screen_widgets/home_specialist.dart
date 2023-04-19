b16dfad30564bb06824602aabc6f6b00f91ff68f
messabih-khalil <msbih.khalil@gmail.com>
2023-04-19 17:36:10 +0100

    refactor(home_specialist.dart): rename HomeCategories to HomeSpecialities feat(home_specialist.dart): add FutureBuilder to fetch specialities from provider and display them in a GridView

--- Mobile/lib/widgets/home_screen_widgets/home_specialist.dart
+++ Mobile/lib/widgets/home_screen_widgets/home_specialist.dart
@@ -1,30 +1,42 @@
+import 'package:app/providers/specialities.dart';
 import 'package:app/widgets/shared/speciality_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeCategories extends StatelessWidget {
-  const HomeCategories({super.key});
+class HomeSpecailities extends StatelessWidget {
+  const HomeSpecailities({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 180,
-          child: GridView.builder(
-            itemCount: 8,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              // childAspectRatio: 0.7,
-            ),
-            itemBuilder: (BuildContext context, int index) {
-              return CategoryCard();
-            },
-          ),
-        ),
-       
-      ],
-    );
+    return FutureBuilder(
+        future: context.watch<Specialities>().getSpecialities(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Column(
+            children: [
+              SizedBox(
+                height: 180,
+                child: GridView.builder(
+                  itemCount: 1,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    mainAxisSpacing: 10.0,
+                    crossAxisSpacing: 10.0,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return SpecialityCard(
+                      speciality:
+                          context.watch<Specialities>().specialities[index],
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        });
   }
 }
