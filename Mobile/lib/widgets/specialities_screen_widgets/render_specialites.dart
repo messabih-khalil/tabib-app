01e339130d4d1c21a169a70b48998e8364a187ff
messabih-khalil <msbih.khalil@gmail.com>
2023-04-19 17:36:36 +0100

    feat(specialities_screen_widgets): add FutureBuilder to fetch specialities from provider and display them in a GridView.

--- Mobile/lib/widgets/specialities_screen_widgets/render_specialites.dart
+++ Mobile/lib/widgets/specialities_screen_widgets/render_specialites.dart
@@ -1,5 +1,7 @@
+import 'package:app/providers/specialities.dart';
 import 'package:app/widgets/shared/speciality_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SpecialitiesCards extends StatelessWidget {
   const SpecialitiesCards({super.key});
@@ -7,20 +9,38 @@ class SpecialitiesCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:
-          MediaQuery.of(context).size.height - AppBar().preferredSize.height,
-      child: GridView.builder(
-        itemCount: 14,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          mainAxisSpacing: 10.0,
-          crossAxisSpacing: 10.0,
-          // childAspectRatio: 0.7,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return CategoryCard();
-        },
-      ),
-    );
+        height:
+            MediaQuery.of(context).size.height - AppBar().preferredSize.height,
+        child: FutureBuilder(
+            future: context.watch<Specialities>().getSpecialities(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Column(
+                children: [
+                  SizedBox(
+                    height: 180,
+                    child: GridView.builder(
+                      itemCount:
+                          context.watch<Specialities>().specialities.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 10.0,
+                        crossAxisSpacing: 10.0,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        return SpecialityCard(
+                          speciality:
+                              context.watch<Specialities>().specialities[index],
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }));
   }
 }
