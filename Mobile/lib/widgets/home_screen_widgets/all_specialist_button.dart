cf4ff73aa8a8ba08058274db1c3ac07607129318
messabih-khalil <msbih.khalil@gmail.com>
2023-04-19 14:32:50 +0100

    feat(home_screen_widgets): add AllSpecialitiesButton and HomeCategories widgets to home screen
    Two new widgets have been added to the home screen. AllSpecialitiesButton is a button that navigates to the SpecialitiesScreen when tapped. HomeCategories is a widget that displays a grid of CategoryCards.

---
+++ Mobile/lib/widgets/home_screen_widgets/all_specialist_button.dart
@@ -0,0 +1,30 @@
+import 'package:app/screens/specialities_screen.dart';
+import 'package:app/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class AllSpecialitiesButton extends StatelessWidget {
+  const AllSpecialitiesButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(SpecialitiesScreen.route);
+      },
+      child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(8),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              border: Border.all(width: 2, color: AppColors.mainDarkColor),
+              borderRadius: BorderRadius.circular(10)),
+          child: Text(
+            "See all",
+            style: TextStyle(
+                fontSize: 18,
+                color: AppColors.mainDarkColor,
+                fontWeight: FontWeight.bold),
+          )),
+    );
+  }
+}
