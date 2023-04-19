b4e69b48e079806c738a54f2229e6c9b97e0dee1
messabih-khalil <msbih.khalil@gmail.com>
2023-04-19 15:13:00 +0100

    feat(http_client.dart): add HttpClient class with get and post methods for making HTTP requests with authorization headers.

---
+++ Mobile/lib/domain/http_client.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class HttpClient with ChangeNotifier {
+  final String? token;
+
+  HttpClient({this.token});
+
+  static final String _baseUrl = 'https://example.com/api/';
+
+  final http.Client _client = http.Client();
+
+  Future<http.Response> get(String url) async {
+    final token = this.token;
+    final headers = {'Authorization': 'Bearer $token'};
+    final response =
+        await _client.get(Uri.parse(_baseUrl + url), headers: headers);
+    return response;
+  }
+
+  Future<http.Response> post(String url,
+      {Map<String, String>? headers, dynamic body}) async {
+    final token = this.token;
+    headers ??= {};
+    headers['Authorization'] = 'Bearer $token';
+    final response = await _client.post(Uri.parse(_baseUrl + url),
+        headers: headers, body: body);
+    return response;
+  }
+
+  
+}
