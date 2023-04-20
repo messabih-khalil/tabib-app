6eaf1737483d86522cb011bafd664b285c746759
messabih-khalil <msbih.khalil@gmail.com>
2023-04-20 22:24:48 +0100

    feat(main.dart): add DoctorsProvider, AppointmentsProvider and related screens to the app. Update SpecialitiesProvider to use ChangeNotifierProxyProvider.

--- Mobile/lib/main.dart
+++ Mobile/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:app/providers/appointments.dart';
 import 'package:app/providers/auth.dart';
+import 'package:app/providers/doctors.dart';
 import 'package:app/providers/specialities.dart';
 import 'package:app/screens/app_home.dart';
+import 'package:app/screens/appointment_screen.dart';
+import 'package:app/screens/doctors_screen.dart';
 import 'package:app/screens/home_screen.dart';
+import 'package:app/screens/my_appointments.dart';
 import 'package:app/screens/signin_screen.dart';
 import 'package:app/screens/signup_screen.dart';
 import 'package:app/screens/specialities_screen.dart';
@@ -21,13 +26,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<Auth>(create: (ctx) => Auth()),
-        ChangeNotifierProxyProvider<Auth, CustomHttpClient>(
-          create: (_) => CustomHttpClient(),
-          update: (context, auth, previous) =>
-              CustomHttpClient(token: auth.token),
+        ChangeNotifierProvider.value(value: Auth()),
+        ChangeNotifierProxyProvider<Auth, Specialities>(
+          create: (_) => Specialities(),
+          update: (context, auth, previous) => Specialities(token: auth.token),
         ),
-        ChangeNotifierProvider<Specialities>(create: (_) => Specialities())
+        ChangeNotifierProxyProvider<Auth, DoctorsProvider>(
+          create: (_) => DoctorsProvider(),
+          update: (context, auth, previous) =>
+              DoctorsProvider(token: auth.token),
+        ),
+        ChangeNotifierProxyProvider<Auth, Appointments>(
+          create: (_) => Appointments(),
+          update: (context, auth, previous) => Appointments(token: auth.token),
+        )
       ],
       child: Consumer<Auth>(
         builder: (_, auth, __) => MaterialApp(
@@ -41,7 +53,10 @@ class MyApp extends StatelessWidget {
             SigninScreen.route: (context) => SigninScreen(),
             SignupScreen.route: (context) => SignupScreen(),
             AppHome.route: (context) => AppHome(),
-            SpecialitiesScreen.route: (context) => SpecialitiesScreen()
+            SpecialitiesScreen.route: (context) => SpecialitiesScreen(),
+            DoctorsScreen.route: (context) => DoctorsScreen(),
+            AppointmentScreen.route: (context) => AppointmentScreen(),
+            MyAppointments.route: (context) => MyAppointments(),
           },
         ),
       ),
