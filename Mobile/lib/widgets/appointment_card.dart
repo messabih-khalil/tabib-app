64eb6381b8a09a0083fa026e664b96754fed1048
messabih-khalil <msbih.khalil@gmail.com>
2023-04-20 22:28:42 +0100

    feat(appointment_card.dart): add AppointmentCard widget to display appointment information

---
+++ Mobile/lib/widgets/appointment_card.dart
@@ -0,0 +1,120 @@
+import 'package:app/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppointmentCard extends StatelessWidget {
+
+  final appointment;
+  const AppointmentCard({this.appointment});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Date",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "22 dec 2023",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Specialist :",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Time",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "13:00",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Dentist",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Doctor",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Dr. Messabih",
+                        style: TextStyle(color: AppColors.lightGreyColor),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text("")
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(6),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 2,
+                            color: const Color.fromARGB(255, 255, 147, 147)),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(
+                      "Cancel",
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: const Color.fromARGB(255, 255, 147, 147),
+                          fontWeight: FontWeight.bold),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
