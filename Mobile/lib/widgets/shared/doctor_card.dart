eddac337dae206a11886c0ff91326de68efe3ef9
messabih-khalil <msbih.khalil@gmail.com>
2023-04-20 22:28:11 +0100

    feat(doctor_card.dart): add doctor object as a parameter to DoctorCard widget and use its properties to display doctor information. Add navigation to AppointmentScreen when the "Book Now" button is pressed.

--- Mobile/lib/widgets/shared/doctor_card.dart
+++ Mobile/lib/widgets/shared/doctor_card.dart
@@ -1,89 +1,94 @@
+import 'package:app/screens/appointment_screen.dart';
 import 'package:app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
 class DoctorCard extends StatelessWidget {
-  const DoctorCard({super.key});
+  final doctor;
+  const DoctorCard({this.doctor});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 100,
-                            width: 100,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Image.network(
-                                'https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=',
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Dr. Messabih",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "Dentist",
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.mainDarkColor),
-                              ),
-                              Text(
-                                "0788558855",
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.lightGreyColor),
-                              ),
-                              Text(
-                                "Chlef, Chlef",
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.lightGreyColor),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                            width: double.infinity,
-                            padding: EdgeInsets.all(10),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: AppColors.mainDarkColor,
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Text(
-                              "Book Now",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold),
-                            )),
-                      ),
-                    ]),
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    doctor['image'],
+                    fit: BoxFit.cover,
                   ),
-                );
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    doctor['username'],
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Dentist",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.mainDarkColor),
+                  ),
+                  Text(
+                    doctor['user']['phoneNumber'],
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.lightGreyColor),
+                  ),
+                  Text(
+                    "Chlef, Chlef",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.lightGreyColor),
+                  ),
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                  AppointmentScreen.route,
+                  arguments: doctor['_id']);
+            },
+            child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: AppColors.mainDarkColor,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  "Book Now",
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )),
+          ),
+        ]),
+      ),
+    );
   }
 }
