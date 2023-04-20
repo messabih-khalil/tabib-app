144eb96eeb969e47fb760186bba8ff39d58372e8
messabih-khalil <msbih.khalil@gmail.com>
2023-04-20 22:27:11 +0100

    feat(doctors_screen.dart): add DoctorsScreen widget to display doctors list based on category ID. Doctors list is fetched from DoctorsProvider.

---
+++ Mobile/lib/screens/doctors_screen.dart
@@ -0,0 +1,46 @@
+import 'package:app/providers/doctors.dart';
+import 'package:app/utils/spaces.dart';
+import 'package:app/widgets/bottom_navigation.dart';
+import 'package:app/widgets/shared/doctor_card.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class DoctorsScreen extends StatelessWidget {
+  const DoctorsScreen({super.key});
+  static const route = '/doctors';
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          args['title'],
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigation(),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: AppSpaces.container),
+        height:
+            MediaQuery.of(context).size.height - AppBar().preferredSize.height,
+        child: FutureBuilder(
+            future: context.watch<DoctorsProvider>().getDoctors(args['id']),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+              return ListView.builder(
+                  itemCount: context.watch<DoctorsProvider>().doctors.length,
+                  itemBuilder: (ctx, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: DoctorCard(doctor : context.watch<DoctorsProvider>().doctors[index]),
+                    );
+                  });
+            }),
+      ),
+    );
+  }
+}
